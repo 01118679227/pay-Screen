7653a22fcb8ed02fffedffd01f54fed95b20e0b4
mahmoud <mh345abd@gmail.com>
2020-06-17 01:22:52 -0700

    ten commit

--- payment/payment_method/README.md
+++
@@ -1,8 +0,0 @@
-# payment_method
-
-A new Flutter project.
-
-## Getting Started
-
-For help getting started with Flutter, view our online
-[documentation](https://flutter.io/).
--- payment/payment_method/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:payment_method/ui.dart';
-
-void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
- home: UI(),
-));
